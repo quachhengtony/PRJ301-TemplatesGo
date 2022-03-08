0cc6a5eadf4b9476817a08cd4b88d02ab48ed49d
Thanh Pham <ptt0412@gmail.com>
2022-03-08 19:48:46 +0700

    Update build file

--- TemplatesGo/src/java/dbmanager/OrderManager.java
+++ TemplatesGo/src/java/dbmanager/OrderManager.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package models;
+package dbmanager;
 
 /**
  *
  * @author Thanh
  */
-public class Account {
+public class OrderManager {
     
 }
