4dd450e52d880d4ffbe6ec1e167772c754c72909
lol02012001 <akiraisora@gmail.com>
2022-03-11 21:32:30 +0700

    Add category and order model

--- TemplatesGo/src/java/models/Category.java
+++ TemplatesGo/src/java/models/Category.java
@@ -10,5 +10,38 @@ package models;
  * @author Thanh
  */
 public class Category {
+    private int id;
+    private String category;
+
+    public Category(int id, String category) {
+        this.id = id;
+        this.category = category;
+    }
+
+    public Category(String category) {
+        this.category = category;
+    }
+    
+    public Category() {
+        this.id = 0;
+        this.category = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
     
 }
